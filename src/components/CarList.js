199583c9290eeb123bd51b1d77d6ebd03f92d0d6
Tahmid Hamim <tahmidhamim@gmail.com>
2024-04-27 12:47:58 +0600

    Init commit

---
+++ src/components/CarList.js
@@ -0,0 +1,47 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { removeCar } from '../store';
+
+function CarList() {
+    const dispatch = useDispatch();
+    const { name, cars } = useSelector(({ form, cars: { searchTerm, data } }) => {
+        const filteredCars = data.filter((car) =>
+            car.name.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+
+        return {
+            name: form.name,
+            cars: filteredCars
+        };
+    });
+
+    const handleCarDelete = (car) => {
+        dispatch(removeCar(car.id));
+    };
+
+    const renderedCars = cars.map((car) => {
+        const bold = name && car.name.toLowerCase().includes(name.toLowerCase());
+
+        return (
+            <div key={car.id} className={`panel ${bold && 'bold'}`}>
+                <p>
+                    {car.name} - ${car.cost}
+                </p>
+                <button
+                    className="button is-danger"
+                    onClick={() => handleCarDelete(car)}
+                >
+                    Delete
+                </button>
+            </div>
+        );
+    });
+
+    return (
+        <div className="car-list">
+            {renderedCars}
+            <hr />
+        </div>
+    );
+}
+
+export default CarList;
